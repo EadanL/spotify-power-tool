6a69addcb69d5c347f2cacda1acb63d5a9032e15
Eadan <eadan.lay@ucalgary.ca>
2026-01-24 21:34:00 -0700

    chore: tracks, playlists, and junction table entries all in DataFrames ready to send to data warehouse

--- spotify_etl.py
+++ spotify_etl.py
@@ -7,6 +7,17 @@ from spotipy.oauth2 import SpotifyOAuth
 load_dotenv()
 
 
+def spotify_etl():
+    liked_tracks = get_all_saved_tacks()
+    playlists = get_all_playlists()
+    playlist_tracks, playlists_cleaned, playlist_track_junction = (
+        get_all_playlist_songs(playlists)
+    )
+    tracks_master = pd.concat([liked_tracks, playlist_tracks], axis=0)
+    tracks_master = tracks_master.loc[~tracks_master.index.duplicated(keep="first"), :]
+    print("done")
+
+
 def get_all_saved_tacks() -> pd.DataFrame | None:
     """
     Retrieves all tracks from users "liked songs"
@@ -33,8 +44,11 @@ def get_all_saved_tacks() -> pd.DataFrame | None:
         tracks.append(item["track"])
 
     tracks_df = pd.DataFrame(tracks)
-    tracks_df.set_index("id")
-    tracks_df = tracks_df.drop(["disc_number", "preview_url", "type"], axis=1)
+    tracks_df = tracks_df.set_index("id")
+    tracks_df = tracks_df.drop(
+        columns=["disc_number", "preview_url", "type", "is_local", "is_playable"],
+        axis=1,
+    )
 
     return tracks_df
 
@@ -53,6 +67,10 @@ def get_all_playlists() -> pd.DataFrame | None:
     ]
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
 
+    # Get current user's ID
+    current_user = sp.current_user()
+    user_id = current_user["id"]
+
     results = sp.current_user_playlists(limit=50)
     if results is None:
         print("Error retrieving all playlists")
@@ -61,17 +79,19 @@ def get_all_playlists() -> pd.DataFrame | None:
 
     while results["next"]:
         for item in results["items"]:
-            playlist = sp.playlist(item["id"])
-            playlists.append(playlist)
+            # Only include playlists created by the current user
+            if item["owner"]["id"] == user_id:
+                playlists.append(item)
 
         results = sp.next(results)
 
     for item in results["items"]:
-        playlist = sp.playlist(item["id"])
-        playlists.append(playlist)
+        # Only include playlists created by the current user
+        if item["owner"]["id"] == user_id:
+            playlists.append(item)
 
     playlists_df = pd.DataFrame(playlists)
-    playlists_df.set_index("id")
+    playlists_df = playlists_df.set_index("id")
     playlists_df = playlists_df.drop(
         ["images", "primary_color", "snapshot_id", "type"], axis=1
     )
@@ -87,20 +107,42 @@ def get_all_playlist_songs(playlists: pd.DataFrame) -> pd.DataFrame | None:
     ]
     sp = spotipy.Spotify(auth_manager=SpotifyOAuth(scope=scope))
     tracks = []
+
     for row in playlists.itertuples():
-        results = sp.playlist_tracks(row.id, limit=100)
+        results = sp.playlist_tracks(row.Index, limit=100)
         while results:
-            tracks.extend(results["items"])
+            # Add track to master track list with playlist_id
+            tracks.extend(
+                [{**item, "playlist_id": row.Index} for item in results["items"]]
+            )
+
             if results["next"]:
                 results = sp.next(results)
             else:
                 break
 
-    tracks_df = pd.DataFrame(tracks)
-    return tracks_df
+    playlist_tracks = pd.DataFrame(tracks)
+
+    # Extract track_id from nested track object to top level
+    playlist_tracks["track_id"] = playlist_tracks["track"].apply(
+        lambda x: x.get("id") if isinstance(x, dict) else None
+    )
+
+    playlist_track_junction = playlist_tracks.drop(
+        columns=["is_local", "primary_color", "track", "video_thumbnail"]
+    )
+
+    playlist_tracks = playlist_tracks["track"].apply(pd.Series)
+    playlist_tracks = playlist_tracks.set_index("id")
+    playlist_tracks = playlist_tracks.drop(
+        columns=["disc_number", "preview_url", "type", "is_local"],
+        axis=1,
+    )
+
+    playlists = playlists.drop(columns=["tracks"])
+
+    return playlist_tracks, playlists, playlist_track_junction
 
 
 if __name__ == "__main__":
-    # get_all_saved_tacks()
-    playlists = get_all_playlists()
-    get_all_playlist_songs(playlists)
+    spotify_etl()
